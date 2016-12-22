fa7322a8445dbb37beb3a9b595710402fd5a738b
Juan Manuel Mougan <juanmougan@gmail.com>
2016-12-21 23:30:45 -0300

    Fetching the real API now

--- song_fetcher.js
+++ song_fetcher.js
@@ -3,29 +3,36 @@
 let fetch = require('node-fetch')
 const API_KEY = process.env.SONGS_API_KEY
 let userName = 'juanma_greenday'
-let randomPageNumber = 123		// TODO this should be a random > 0
+let threshold = 7220
+let randomPageNumber = Math.floor(Math.random() * threshold) + 1
+console.log("Will get song # " + randomPageNumber)
 // TODO returns this
 /*
 "totalPages": "7220" 
 */
 // I should use that as random's threshold
-let baseUrl = 'http://ws.audioscrobbler.com/2.0/?method=user.gettoptracks&user=' + userName + '&api_key=' + API_KEY +'&limit=1&page=' + randomPageNumber + '&format=json'
+let url = 'http://ws.audioscrobbler.com/2.0/?method=user.gettoptracks&user=' + userName + '&api_key=' + API_KEY +'&limit=1&page=' + randomPageNumber + '&format=json'
 
-/*
 var getSong = (err, success) => {
   fetch(url)
     .then((res) => { return res.json() })
     .then((data) => {
+      console.log("Got this: " + data)
+      let title = data.toptracks.track[0].name
+      let artist = data.toptracks.track[0].artist.name
+      console.log("Got " + title + " by " + artist)
+      success(title, artist)
     })
     .catch((error) => {
       err(error)
     })
 }
-*/
 
+/*
 var getSong = (err, success) => {
   success("Let It Be", "The Beatles")
 }
+*/
 
 module.exports = {
   getSong : getSong
