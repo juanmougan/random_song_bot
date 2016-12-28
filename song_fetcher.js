6e9d5ec28461a76bda22dda3bb43606a47cda3ac
Juan Manuel Mougan <juanmougan@gmail.com>
2016-12-28 20:27:36 -0300

    Fix #4: Added a first API call to get the threshold

--- song_fetcher.js
+++ song_fetcher.js
@@ -5,28 +5,41 @@ let fetch = require('node-fetch')
 const API_KEY = process.env.SONGS_API_KEY
 let threshold = 7220
 
-// TODO returns this
-/*
-"totalPages": "7220" 
-*/
-// I should use that as random's threshold
-
-var getSong = (err, userName, success) => {
-  let randomPageNumber = Math.floor(Math.random() * threshold) + 1
-  console.log("Will get song # " + randomPageNumber)
-  let url = 'http://ws.audioscrobbler.com/2.0/?method=user.gettoptracks&user=' + userName + '&api_key=' + API_KEY +'&limit=1&page=' + randomPageNumber + '&format=json'
-  fetch(url)
-    .then((res) => { 
-      return res.json() })
+var getSongsThreshold = (err, userName, success) => {
+  let onePageUrl = 'http://ws.audioscrobbler.com/2.0/?method=user.gettoptracks&user=' 
+    + userName + '&api_key=' + API_KEY +'&limit=1&page=1&format=json'
+  fetch(onePageUrl)
+    .then((res) => {
+      return res.json()
+    })
     .then((body) => {
-      let title = body.toptracks.track[0].name
-      let artist = body.toptracks.track[0].artist.name
-      console.log("Got " + title + " by " + artist)
-      success(title, artist)
+      success(body.toptracks['@attr'].totalPages)
     })
     .catch((error) => {
       err(error)
     })
+}
+
+var getSong = (err, userName, success) => {
+  getSongsThreshold(err, userName, (thresholdSuccess) => {
+  	console.log("Threshold set to: " + thresholdSuccess)
+  	let randomPageNumber = Math.floor(Math.random() * thresholdSuccess) + 1
+  	console.log("Will get song # " + randomPageNumber)
+    let url = 'http://ws.audioscrobbler.com/2.0/?method=user.gettoptracks&user=' + userName + '&api_key=' + API_KEY +'&limit=1&page=' + randomPageNumber + '&format=json'
+    fetch(url)
+      .then((res) => { 
+        return res.json()
+      })
+      .then((body) => {
+        let title = body.toptracks.track[0].name
+        let artist = body.toptracks.track[0].artist.name
+        console.log("Got " + title + " by " + artist)
+        success(title, artist)
+      })
+      .catch((error) => {
+        err(error)
+      })
+  })  
 }
 
 module.exports = {
