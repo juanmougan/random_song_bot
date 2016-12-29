c31bfbd1757afb65f8f94b7ec3a8c805d0a4e7a2
Juan Manuel Mougan <juanmougan@gmail.com>
2016-12-28 22:56:16 -0300

    Heroku setup

--- web.js
+++ web.js
@@ -1,14 +1,14 @@
-var express = require('express');
+var express = require('express')
 
-var app = express();
+var app = express()
 
 app.get('/', function (req, res) {
-  res.json({ "hello": "Welcome to the random_song_bot!" });
-});
+  res.json({ "hello": "Welcome to the random_song_bot!" })
+})
 
 var server = app.listen(process.env.PORT, function () {
-  var host = server.address().address;
-  var port = server.address().port;
+  var host = server.address().address
+  var port = server.address().port
 
-  console.log('Web server started at http://%s:%s', host, port);
-});
+  console.log('Web server started at http://%s:%s', host, port)
+})
